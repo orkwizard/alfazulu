0395957835f4417ce5663760e910abf829a77c33
Javier Gonzalez <jgrodriguez.pro@gmail.com>
2022-03-09 23:47:49 -0500

    remove postfakelogin

--- src/store/auth/login/saga.js
+++ src/store/auth/login/saga.js
@@ -19,10 +19,19 @@ function* loginUser({ payload: { user, history } }) {
         localStorage.setItem("authUser", JSON.stringify(response));
         yield put(loginSuccess(response));
       } else if (process.env.REACT_APP_DEFAULTAUTH === "fake") {
-        const response = yield call(postFakeLogin, {
-          email: user.email,
-          password: user.password,
-        });
+        // const response = yield call(postFakeLogin, {
+        //   email: user.email,
+        //   password: user.password,
+        // });
+
+        let response = {
+          email: "demo@demo.com",
+          password: "123456",
+          role: "admin",
+          uid: 1,
+          username: "Demo",
+        }
+
         localStorage.setItem("authUser", JSON.stringify(response));
         yield put(loginSuccess(response));
       }
